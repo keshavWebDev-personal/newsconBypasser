273b0dd9690ad529df8cb11d9dfb089445b4ccea
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2023-09-10 23:36:11 +0530

    Add files via upload

---
+++ dist/popup.js
@@ -0,0 +1,50 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const textBox = document.getElementById('text-box');
+    if (!textBox)
+        return;
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        if (tabs[0].url === undefined || tabs[0].id === undefined) {
+            return;
+        }
+        if (tabs[0].url.includes("https://www.newscon.org/download/")) {
+            textBox.innerText = `window.open(new URL(downloadURL), "_self")`;
+        }
+        else if (tabs[0].url.includes("https://www.newscon.org/snippet/")) {
+            textBox.innerText = `
+            remainingTime = 0;
+            setTimeout(() => {
+                var textContent = document.getElementById("btnCopy")?.querySelector("div")?.innerText;
+                textContent ? window.open(new URL(textContent), "_self"): null ;
+            
+            }, 1000);`;
+        }
+    });
+    textBox.addEventListener('click', () => {
+        const tempTextarea = document.createElement('textarea');
+        if (!textBox.textContent)
+            return;
+        tempTextarea.value = textBox.textContent;
+        document.body.appendChild(tempTextarea);
+        // Select the text inside the textarea and copy it to the clipboard
+        tempTextarea.select();
+        document.execCommand('copy');
+        // Remove the temporary textarea
+        document.body.removeChild(tempTextarea);
+        // Add a class to indicate successful copy
+        textBox.classList.add('copied');
+        // Remove the class after a delay
+        setTimeout(() => {
+            textBox.classList.remove('copied');
+        }, 2000);
+    });
+});
+// if (window.downloadURL) {
+//     window.open(new URL(downloadURL), "_self")
+// } else if (window.remainingTime) {
+//     remainingTime = 0
+//     setTimeout(() => {
+//         var textContent = document.getElementById("btnCopy")?.querySelector("div")?.innerText;
+//         textContent ? window.open(new URL(textContent), "_self") : null;
+//     }, 1000);
+// }
